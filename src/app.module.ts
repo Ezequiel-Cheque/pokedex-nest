e347d5702e374aea4b61e0100eafd3afd0feab67
Ezequiel-Cheque <delaluzezequiel@gmail.com>
2024-03-21 00:55:49 -0600

    FEAT: add GET, Create, PUT services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 
 @Module({
@@ -9,6 +10,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot("mongodb://localhost:27018/nest-pokemon"),
     PokemonModule
   ],
   controllers: [],
--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsInt, IsNumber, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+    
+    @IsString()
+    @MinLength(1)
+    name: string;
+
+    @Min(1)
+    @IsNumber()
+    @IsInt()
+    no: number;
+}
