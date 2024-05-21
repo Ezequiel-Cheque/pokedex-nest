6adf64fcd5e709f6ab104acbc5076351fc4f1c04
Ezequiel-cheque <delaluzezequiel@gmail.com>
2024-05-20 18:57:04 -0600

    FEAT: still seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interfaces';
-
+import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
   
   private readonly axios: AxiosInstance = axios;
+  
+  constructor(
+    private readonly pokemonService: PokemonService
+  ){}
 
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>("https://pokeapi.co/api/v2/pokemon?limit=10");
-    data.results.forEach(({ name, url }) => {
-
+    
+    data.results.forEach(async ({ name, url }) => {
+      
       const segments = url.split('/');
       const no = +segments[segments.length -2]
-      console.log({name, no})
+      
+      try {
+        const newPokemon = await this.pokemonService.create({name, no});
+        console.log(newPokemon)
+      } catch (error) {
+        console.log("Error");
+      }
+    
     });
-    return data.results;
+    
+    return "Done";
   }
 
   
